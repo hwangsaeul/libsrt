05aba6a6cb32a9ca8a22206c817da01355d8e43b
Federico Ceratto <federico.ceratto@gmail.com>
2018-09-16 17:07:48 +0100

    Update packaging

---
+++ debian/conf.py
@@ -0,0 +1,17 @@
+# sphinx configuration file
+project = 'SRT'
+copyright = '2018, Haivision Systems Inc'
+author = 'Haivision Systems Inc'
+version = ''
+release = ''
+extensions = [
+]
+templates_path = ['_templates']
+source_suffix = '.md'
+master_doc = 'API'
+exclude_patterns = ['_build', '_themes']
+pygments_style = 'sphinx'
+html_static_path = []
+html_use_index = False
+html_show_sourcelink = False
+htmlhelp_basename = 'SRT'
